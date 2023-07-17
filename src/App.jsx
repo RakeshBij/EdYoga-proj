c7ff09979bce9de3e46af92e705386638f6f1d5b
rakesh bijarniya <rb.firez@gmail.com>
2023-07-17 22:24:34 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { MainSection } from "./components/MainSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MainSection />
+    </>
+  );
+}
+
+export default App;
